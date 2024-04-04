5d75ecec324867fff13be84c1aa064663e88b5a5
fideldavid11 <113791442+fideldavid11@users.noreply.github.com>
2024-04-04 00:38:49 -0400

    Update views.py

--- myproject/views.py
+++ myproject/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
 
-    return HttpResponse("¡Hola, mundo!.")
-
-    return HttpResponse("¡Hola, mundo! Soy Fidel David!.")
+    return HttpResponse("¡Hola, mundo!!!!!")
+    return HttpResponse("¡Hola, mundo! Soy Fidel Pineda!")
 
